aedc6b28423c2d9845f39fee3325e4eaf803249a
Ophir LOJKINE <pere.jobs@gmail.com>
2015-08-05 19:19:23 +0200

    Affichage de la liste des condensateurs

--- condos.js
+++ condos.js
@@ -1,10 +1,13 @@
+var APIURL = "http://api.appheb.erdf-grdf.fr:8520/api.php/";
+
 angular.module('condosApp', [])
-.controller('CondosController', function () {
+.controller('CondosController', function ($http) {
   var condos = this;
-  condos.condos = [
-    {ps:'ALLOUETTE', nom:"CONDO A"},
-    {ps:'ARPAJON', nom:"CONDO B"},
-  ];
+  condos.condos = [];
+  $http.get(APIURL + "liste_condensateurs").then(function listecondos(response) {
+    condos.condos = response.data.results;
+    console.log(response);
+  });
   condos.zones = [
       {nom:"Essonne"},
       {nom:"Pantin"},
